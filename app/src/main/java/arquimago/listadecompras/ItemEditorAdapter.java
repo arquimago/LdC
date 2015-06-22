143cd4f5eba0be633d04b67a5c23dbd53d39a9cc
arquimago <thales@alvesnet.net>
2015-06-21 23:34:38 -0300

    XML + Java de edição

---
+++ app/src/main/java/arquimago/listadecompras/ItemEditorAdapter.java
@@ -0,0 +1,67 @@
+package arquimago.listadecompras;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Th on 21/06/2015.
+ */
+public class ItemEditorAdapter extends BaseAdapter {
+
+    private Context contexto;
+    private ArrayList<Item> lista;
+
+    public ItemEditorAdapter(Context contexto, ArrayList<Item> lista){
+        this.contexto=contexto;
+        this.lista=lista;
+    }
+
+    @Override
+    public int getCount(){
+        return lista.size();
+    }
+
+    @Override
+    public Object getItem(int posicao){
+        return lista.get(posicao);
+    }
+
+    @Override
+    public long getItemId(int posicao) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int posicao, View convertView, ViewGroup parent){
+        final Item item = lista.get(posicao);
+        final View layout;
+
+        if(convertView == null){
+            LayoutInflater inflater = (LayoutInflater) contexto.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            layout = inflater.inflate(R.layout.item_editor, null);
+        } else {
+            layout = convertView;
+        }
+
+        TextView nome = (TextView) layout.findViewById(R.id.nome);
+        nome.setText(item.getNome());
+
+        Button editar = (Button) layout.findViewById(R.id.edit);
+        Button deletar = (Button) layout.findViewById(R.id.del);
+
+
+
+
+
+        return layout;
+    }
+}
+
